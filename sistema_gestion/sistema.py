70816ef512f47dd3c323ad09d6bc8a4068763d0d
alexstiven26 <alexstiven420@gmail.com>
2026-05-11 13:56:40 -0500

    Update sistema.py

--- sistema_gestion/sistema.py
+++ sistema_gestion/sistema.py
@@ -1,4 +1,7 @@
-
+# Nombre del estudiante: Alex Stiven Ordoñez Solano
+# Grupo: 213023_254
+# Programa: Ingeniería de Sistemas
+# Código Fuente: autoría propia
 
 from cliente import Cliente
 from servicio import ReservaSala, AlquilerEquipo, AsesoriaEspecializada
